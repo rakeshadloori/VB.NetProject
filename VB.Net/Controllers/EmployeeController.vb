992a89e09fa561d56a28510a4d7c6888d9fbd77b
rakeshadloori <rakeshadloori@gmail.com>
2017-05-16 00:48:34 -0400

    Added Repo and BO

---
+++ VB.Net/Controllers/EmployeeController.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class EmployeeController
+        Inherits Controller
+
+        ' GET: Employee
+        Function Index1() As ActionResult
+
+            Return View()
+        End Function
+
+        Function Index2() As ActionResult
+
+            Return View()
+        End Function
+
+        Function Index3() As ActionResult
+
+            Return View()
+        End Function
+
+        Function Index4() As ActionResult
+
+            Return View()
+        End Function
+    End Class
+End Namespace
